60a7aea71609e0e222f980fbde3c744f91481ad7
Margarita Novokh <r28611@gmail.com>
2021-10-15 21:54:51 +0200

    initial commit

--- TaskComposite/ViewController.swift
+++ TaskComposite/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let taskTable = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view = taskTable
+        taskTable.dataSource = self
     }
 
 
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
+    }
+    
+    
 }
 
